f2c4d7febe80fb13575c0ad0d253bcbfce4ff393
anassuv <114801727+anassuv@users.noreply.github.com>
2022-11-06 09:20:56 +0300

    z15

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите цифру: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n == 6 || n == 7)
+            {
+                Console.WriteLine("да");
+            }
+            else
+            {                
+                Console.WriteLine("нет");
+            }
+            Console.ReadKey();
+        }
+    }
+}
